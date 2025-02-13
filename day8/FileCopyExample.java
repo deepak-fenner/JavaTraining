3ac1ff6e035cd0b676a193ea2cc2339e3b8bdcc3
deepak-fenner <deepakfenner@gmail.com>
2025-02-13 15:28:09 +0530

    new commit

---
+++ day8/FileCopyExample.java
@@ -0,0 +1,21 @@
+package com.celcom.day8;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileCopyExample {
+
+	public static void main(String[] args) throws IOException {
+		FileReader fr=new FileReader("D://Example.txt");
+		FileWriter fw=new FileWriter("D://Example1.txt");
+		int ch=0;
+		while((ch=fr.read())!=-1) {
+			fw.write(ch);
+		}
+		fr.close();
+		fw.close();
+		System.out.println("File Copied");
+	}
+
+}
